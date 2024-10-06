bb99f7fd632a3faa277fa3ae2b1fb52c86d7bd8e
Chitipat Takdumrongkul <6610685130@cn103>
2024-10-06 21:11:07 +0700

    First push

--- RegCN/views.py
+++ RegCN/views.py
@@ -1,3 +1,11 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def subjects(request):
+    return render(request, 'RegCN/MySubjects.html')
+
+def subject(request, subject_id):
+    return HttpResponse('subject ID= ' + str(subject_id))
